7264c33195ba0dd9a5ed5aa9f7834972214012ff
maho <72187585+maho0705@users.noreply.github.com>
2021-06-30 08:43:32 +0900

    Create sim.v

---
+++ sim.v
@@ -0,0 +1,16 @@
+module simcpu;
+   reg clk, rstd;
+   initial begin
+      clk <= 0;
+      forever #50 clk <= ~clk;
+   end
+
+   initial begin
+      rstd <= 1;
+      #10 rstd <= 0;
+      #10 rstd <= 1;
+      #4000 $finish;
+   end
+
+   computer computer_body(clk, rstd);
+endmodule
